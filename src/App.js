051eeb45f2c617c3b674b8d4ce944ae3e08d2c6e
GiorgioRindonone <giorgio.rindonone@gmail.com>
2022-04-21 11:38:15 +0200

    form builder implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,345 @@
-import logo from './logo.svg';
-import './App.css';
+import $ from "jquery";
+import React, {
+  createRef,
+  useEffect,
+  useState,
+  useLayoutEffect,
+  useRef,
+  useCallback
+} from "react";
+import "./App.css";
 
-function App() {
+window.jQuery = $;
+window.$ = $;
+
+require("jquery-ui-sortable");
+require("formBuilder");
+require("formBuilder/dist/form-render.min.js");
+
+const initFormState = {
+  formData: {},
+  formCompiledData: {},
+  formMode: ""
+};
+
+function formReducer(state, action) {
+  //? gli passo action come argomento perchè poi nello switch lui lo usa e sa cosa fare
+  switch (action.type) {
+    case "initFormData":
+      return {
+        formData: action.data,
+        formMode: "form-builder" //init form data
+      };
+
+    case "upFormData":
+      return {
+        ...state,
+        formData: action.data,
+        formMode: "form-builder" //update form data
+      };
+    case "upFormCompiledData":
+      return {
+        ...state,
+        formCompiledData: action.data,
+        formMode: "form-builded" //update form data
+      };
+    default:
+      break;
+  }
+}
+
+export function App() {
+  const fb = useRef();
+  const [form, setForm] = useState(null);
+  const [pedalini, setPedalini] = useState(null);
+  
+  const formData = [
+    {
+      type: "header",
+      subtype: "h1",
+      label: "ffffff"
+    },
+    {
+      type: "paragraph",
+      subtype: "p",
+      label:
+        "This is a demonstration of formBuilder running in a React project."
+    },
+    {
+      type: "radio-group",
+      label: "Radio Group",
+      name: "radio-group-1649668789730",
+      values: [
+        {
+          label: "Option 1",
+          value: "option-1"
+        },
+        {
+          label: "Option 2",
+          value: "option-2"
+        },
+        {
+          label: "Option 3",
+          value: "option-3"
+        }
+      ]
+    },
+    {
+      type: "select",
+      label: "Select",
+      className: "form-control",
+      name: "select-1649668791454",
+      values: [
+        {
+          label: "Option 1",
+          value: "option-1",
+          selected: true
+        },
+        {
+          label: "Option 2",
+          value: "option-2"
+        },
+        {
+          label: "Option 3",
+          value: "option-3"
+        }
+      ]
+    },
+    {
+      type: "text",
+      label: "Text Field",
+      className: "form-control",
+      name: "text-1649668791913",
+      subtype: "text"
+    },
+    {
+      type: "text",
+      label: "Text Field",
+      className: "form-control",
+      name: "text-1649668797027",
+      subtype: "text"
+    },
+    {
+      type: "select",
+      label: "Select",
+      className: "form-control",
+      name: "select-1649668797648",
+      values: [
+        {
+          label: "Option 1",
+          value: "option-1",
+          selected: true
+        },
+        {
+          label: "Option 2",
+          value: "option-2"
+        },
+        {
+          label: "Option 3",
+          value: "option-3"
+        }
+      ]
+    }
+  ];
+  
+  let formBuilder;
+  let options2 = {
+    //append: '<button>Submit</button>',
+    disableFields: ["button", "header", "paragraph", "hidden"],
+    disabledActionButtons: ["save"],
+    editOnAdd: true,
+    onClearAll: function (formData) {
+      setForm(null);
+    },
+    formData: formData,
+    onCloseFieldEdit: function (formData) {
+      console.log("a field edit panel was closed", formData);
+      localStorage.setItem(
+        "form-editing",
+        formBuilder.actions.getData("json", true)
+      );
+    },
+    disabledAttrs: [
+      "placeholder",
+      "inline",
+      "step",
+      //'subtype',
+      "description",
+      "className",
+      "required",
+      "access",
+      "other"
+    ],
+    actionButtons: [
+      {
+        id: "seeform",
+        className: "btn btn-primary",
+        label: "See Form",
+        type: "button",
+        events: {
+          click: function () {
+            $(() => {
+              let myxml = formBuilder.actions.getData("json", true);
+              //let myxml = ilForm.actions.getData("json", true);
+              let submit_btn = {
+                type: "button",
+                label: "Submit",
+                subtype: "submit",
+                className: "btn-light btn",
+                name: "submituniquebutton",
+                access: false,
+                style: "light"
+              };
+              let myxml2 = JSON.parse(myxml);
+              if (myxml2.length > 0) {
+                setForm(myxml);
+
+                var fbTemplate = document.getElementById("build-wrap"),
+                  options = {
+                    formData: myxml2.concat([submit_btn])
+                  };
+
+                $(fbTemplate).formRender(options);
+              } else {
+                alert("Add atleast one Field");
+              }
+            });
+          }
+        }
+      }
+    ],
+    fields: [
+      {
+        label: "File",
+        type: "file",
+        subtype: "file",
+        icon: "✉"
+      }
+    ],
+    controlConfig: {
+      "file.fineuploader": {
+        autoUpload: true
+        // other fine uploader configuration options here
+      }
+    }
+  };
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    (async function () {
+      //seIlForm($(fb.current).formBuilder(options2));
+      //const formBuilder = $(fb.current).formBuilder(options2);
+      formBuilder = $(fb.current).formBuilder(options2);
+      console.log("formBuilder useEffect", formBuilder);
+    })();
+  }, []);
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    //target dell'oggetto
+    var fbTemplate = document.getElementById("build-wrap");
+    // attacco all'oggetto il formRender
+    let userData = $(fbTemplate).formRender("userData");
+    console.log(userData);
+    var formData = new FormData(fbTemplate);
+
+    function getObj(objs, key, val) {
+      val = val.replace("[]", "");
+      return objs.filter(function (obj) {
+        var filter = false;
+        if (val) {
+          filter = obj[key] === val;
+        } else if (obj[key]) {
+          filter = true;
+        }
+        return filter;
+      });
+    }
+
+    function setValue(name, value) {
+      let field = getObj(userData, "name", name)[0];
+
+      if (!field) {
+        return;
+      }
+
+      if (
+        ["select", "checkbox-group", "radio-group"].indexOf(field.type) !== -1
+      ) {
+        for (var fieldOption of field.values) {
+          if (value.indexOf(fieldOption.value) !== -1) {
+            fieldOption.selected = true;
+          } else {
+            delete fieldOption.selected;
+          }
+        }
+      } else {
+        field.value = value[0];
+      }
+    }
+
+    console.log("Original formData: ", userData);
+
+    for (var key of formData.keys()) {
+      setValue(key, formData.getAll(key));
+    }
+
+    console.log("Updated formData: ", userData);
+
+    let toSendData = {};
+    userData.forEach((item) => {
+      if (item.type === "button" && item.subtype === "submit") {
+        return 0;
+      } else if (item.type === "file" || item.subtype === "file") {
+        let field = item.label;
+        let value = item.value;
+        toSendData[field] = value;
+      } else {
+        let field = item.label;
+        let value = item.userData[0];
+        toSendData[field] = value;
+        console.log(toSendData);
+      }
+    });
+  };
+
+  let sandali;
+
+  const showPedalini = () => {
+    //e.preventDefault();
+    var fbTemplate = document.getElementById("fb-editor");
+    //const formBuilder = $(fb.current).formBuilder.actions.getData;
+    //console.log("pedalini", formBuilder.actions.getData("json", true));
+    sandali = formBuilder.actions.getData("json", true);
+    console.log("sandali", sandali);
+    //calzini(sandali);
+    console.log("formData", formData);
+
+    console.log("pedalini 2", pedalini);
+  };
+
+  const calzini = (arg) => {
+    setPedalini(arg);
+    console.log("pedalini", arg);
+    console.log("formData", formData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-4">
+      <div id="fb-editor" ref={fb} />
+      <button id="pedalino" onClick={showPedalini}>
+        PEDALINAMI
+      </button>
+      {form && (
+        <form
+          id="build-wrap"
+          className="pt-3"
+          onSubmit={(e) => formSubmitHandler(e)}
+        />
+      )}
     </div>
   );
 }
 
+//export default MemoisedApp = React.memo(App);
 export default App;
---
+++ src/FormBuilder.js
@@ -0,0 +1,189 @@
+import $ from "jquery";
+import * as React from "react";
+
+(window).jQuery = $;
+(window).$ = $;
+
+require("jquery-ui-sortable");
+require("formBuilder");
+
+const formData = [
+    {
+        "type": "header",
+        "subtype": "h1",
+        "label": "ffffff"
+    },
+    {
+        "type": "paragraph",
+        "subtype": "p",
+        "label": "This is a demonstration of formBuilder running in a React project."
+    },
+    {
+        "type": "radio-group",
+        "label": "Radio Group",
+        "name": "radio-group-1649668789730",
+        "values": [
+            {
+                "label": "Option 1",
+                "value": "option-1"
+            },
+            {
+                "label": "Option 2",
+                "value": "option-2"
+            },
+            {
+                "label": "Option 3",
+                "value": "option-3"
+            }
+        ]
+    },
+    {
+        "type": "select",
+        "label": "Select",
+        "className": "form-control",
+        "name": "select-1649668791454",
+        "values": [
+            {
+                "label": "Option 1",
+                "value": "option-1",
+                "selected": true
+            },
+            {
+                "label": "Option 2",
+                "value": "option-2"
+            },
+            {
+                "label": "Option 3",
+                "value": "option-3"
+            }
+        ]
+    },
+    {
+        "type": "text",
+        "label": "Text Field",
+        "className": "form-control",
+        "name": "text-1649668791913",
+        "subtype": "text"
+    },
+
+
+];
+
+
+
+function FormBuilder() {
+    let formBuilder;
+
+    const formBuilderRef = React.useRef();
+    
+    const options = {
+        disabledActionButtons: ['data', 'clear'],
+        scrollToFieldOnAdd: false,
+        disabledAttrs: [
+            'placeholder',
+            'inline',
+            'step',
+            //'subtype',
+            'description',
+            'className',
+            'required',
+            'access',
+            'other',
+        ],
+        dataType: 'json',
+        formData: formData,
+        editOnAdd: true,
+        //qui salvo i fields direttamente alla chiusura dell'edit 
+        onCloseFieldEdit: function (formData) {
+            console.log('a field edit panel was closed', formData);
+            localStorage.setItem("form-editing", formBuilder.actions.getData("json", true));
+        },
+        disableFields: [
+            'autocomplete',
+            'file',
+            'starRating',
+            'hidden',
+            'button',
+        ],
+        disabledSubtypes: {
+            text: ['password', 'output'],
+        },
+        fields: [
+            {
+                label: "File",
+                type: "file",
+                subtype: "file",
+                icon: "✉"
+            },
+            {
+                label: "Email",
+                type: "text",
+                subtype: "email",
+                icon: "✉"
+            },
+            {
+                label: "Formula Box",
+                name: "Formula",
+                type: "text",
+                subtype: "Text Field",
+                icon: "&"
+            },
+        ],
+        controlConfig: {
+            "file.fineuploader": {
+                autoUpload: true
+                // other fine uploader configuration options here
+            }
+        },
+        onSave: function (evt, formGridData) {
+            // toggleEdit();
+            let data = JSON.stringify(formGridData);
+            // alert(JSON.stringify(formGridData));
+            // setFormGridDataEdit(data);
+            console.log(data);
+            // $('.fb-editor').formRender({ formGridData });
+            window.sessionStorage.setItem('formDataCreated', formGridData);
+        },
+        // stickyControls: {
+        //     enable: true,
+        // },
+        // actionButtons: [{
+        //     id: 'render-form-builder',
+        //     className: 'btn btn-success',
+        //     label: 'Preview',
+        //     type: 'button',
+        //     events: {
+        //         click: function (evt, formGridData) {
+        //             $('.fb-editor').formRender().toggleEdit();
+        //             $('.fb-editor').formRender({ formGridData });
+        //             window.sessionStorage.setItem('formGridData', JSON.stringify(formGridData));
+        //         },
+        //     }
+        // }],
+
+        // layoutTemplates: {
+        //     help: function (helpText) {
+        //         return $('<div/>')
+        //             .addClass('help')
+        //             .append(helpText);
+        //     },
+        //     label: function (label, data) {
+        //         // cheeky styling
+        //         return $('<label class="bright" style="margin-top:15px;"/>')
+        //             .attr('for', data.id)
+        //             .append(label);
+        //     }
+        // },
+    };
+
+    formBuilder = $(formBuilderRef.current).formBuilder(options);
+
+
+    const renderData = () => {
+        return <div id="form-builder-container" ref={formBuilderRef} />;
+    };
+
+    return renderData();
+};
+
+export default FormBuilder;
